f829c4c05f989671533d7c8df16f17d99b8cff89
basicjohn <edmondsonj@gmail.com>
2021-03-24 10:51:48 -0700

    created the script and fixed errors
    Co-authored-by: Maxwell Meyer <maxreadswell@gmail.com>
    Co-authored-by: Noah Woodrum <heymymomthinksimcool@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+/*$(document).ready(function () {
   $("#formOne").submit(function (event) {
     const person1Input = $("input#person1").val();
     const person2Input = $("input#person2").val();
@@ -13,6 +13,32 @@ $(document).ready(function () {
     $(".exclamation").text(exclamationInput);
     $(".verb").text(verbInput);
     $(".noun").text(nounInput);
+
+    $("#story").show();
+
+    event.preventDefault();
+  });
+});*/
+
+$(document).ready(function () {
+  $("#formOne").submit(function (event) {
+    const noun = $("input#noun").val();
+    const pluralNoun = $("input#pluralNoun").val();
+    const presentTenseVerb = $("input#presentTenseVerb").val();
+    const anotherPresentTenseVerb = $("input#anotherPresentTenseVerb").val();
+    const partOfBodyPlural = $("input#partOfBodyPlural").val();
+    const adj = $("input#adjective").val();
+    const anotherPluralNoun = $("input#anotherPluralNoun").val();
+    const anotherAdjective = $("input#anotherAdjective").val();
+
+    $(".noun").text(noun);
+    $(".pluralNoun").text(pluralNoun);
+    $(".presentTenseVerb").text(presentTenseVerb);
+    $(".anotherPresentTenseVerb").text(anotherPresentTenseVerb);
+    $(".partOfBodyPlural").text(partOfBodyPlural);
+    $(".adjective").text(adj);
+    $(".anotherPluralNoun").text(anotherPluralNoun);
+    $(".anotherAdjective").text(anotherAdjective);
 
     $("#story").show();
 
